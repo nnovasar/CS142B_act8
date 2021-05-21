796a401d5f3bceca39dc64ae1ca1b752f7252774
nnovasar <38850106+nnovasar@users.noreply.github.com>
2021-05-21 04:52:10 -0700

    Create testInst.cpp

---
+++ testInst.cpp
@@ -0,0 +1,40 @@
+// Look at the testInst.cpp file provided in the GEN repository and write Instructions for the following Mini-py functions
+
+/* Part a
+
+def main():
+    i: int = 10
+    s: int = 0
+    while i >= 0:
+        s = s + i
+        i = i - 1
+
+*/
+
+/* Part b
+
+def is_even(i:int)->bool:
+    if i % 2 == 0:
+        return True
+    return False
+
+def main():
+    is_even(5)
+
+
+*/
+
+/* Part c
+
+def divisible(i:int)->bool:
+    if i % 3 == 0 and i % 5 == 0:
+        return False
+    elif i % 3 == 0 or i % 5 == 0:
+        return True
+    else:
+        return False
+
+def main():
+    divisible(35)
+
+*/
